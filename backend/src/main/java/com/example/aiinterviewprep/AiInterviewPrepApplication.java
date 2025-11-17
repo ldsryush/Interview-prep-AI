6a01bba16478d708c0194c731c16937f2f646112
ldsryush <ldsryush@gmail.com>
2025-11-16 19:24:35 -0700

    implemented postgreSQL

---
+++ backend/src/main/java/com/example/aiinterviewprep/AiInterviewPrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiinterviewprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewPrepApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/aiinterviewprep/AiInterviewPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiinterviewprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
